6c5c41e7498019db80616e57068a5d9ef46abc10
Jeff Seymour and Lauryn Davis <lryndavis@gmail.com>
2016-01-05 10:12:46 -0800

    passes 7th spec

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -20,7 +20,7 @@ describe('String#scrabble') do
   it("returns a scrabble score for a letter") do
     expect("j".scrabble()).to(eq(8))
   end
-  # it("returns a scrabble score for a letter") do
-  #   expect("q".scrabble()).to(eq(10))
-  # end
+  it("returns a scrabble score for a letter") do
+    expect("q".scrabble()).to(eq(10))
+  end
 end
